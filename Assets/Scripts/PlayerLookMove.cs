bef810f7c57679a071bf9291a462ecb6bea0dab1
Vertti Viitanen <vertti.viitanen@gmail.com>
2016-01-20 19:27:24 +0200

    Sticky grab

--- Assets/Scripts/PlayerLookMove.cs
+++ Assets/Scripts/PlayerLookMove.cs
@@ -4,116 +4,158 @@ using System.Collections;
 
 public class PlayerLookMove : MonoBehaviour {
 
-	bool mouseLock;
-	bool launched;
-	public float maxGrappleDist;
-	public float movSpeed;
-	public float wasdSpeed;
-	public float mouseSens;
-	public float upDownRange;
-	float verticalRotation;
-	float horizontalRotation;
-	Vector3 launchDir;
-	Vector3 playerToObject;
-	Vector3 wsDir;
-	Vector3 adDir;
-	public GameObject mainCam;
-	GameObject attachedToObject;
-	public Slider mouseSlider;
-	public Text mouseText;
-	RaycastHit hitPoint;
+    bool mouseLock;
+    bool launched;
+    public float maxGrappleDist;
+    public float movSpeed;
+    public float wasdSpeed;
+    public float mouseSens;
+    public float upDownRange;
+    float verticalRotation;
+    float horizontalRotation;
+    Vector3 launchDir;
+    Vector3 playerToObject;
+    Vector3 wsDir;
+    Vector3 adDir;
+    public GameObject mainCam;
+    public Slider mouseSlider;
+    public Text mouseText;
+    RaycastHit hitPoint;
 
-	void Start () {
-		MouseLock();
+    float radius = 1.0f;
+
+    Transform attachedTo;
+    Vector3 attachDir;
+    Vector3 attachPos;
+    Vector3 attachScale;
+    Quaternion attachRot;
+
+    void Start() {
+        MouseLock();
+    }
+
+    void Update() {
+        if (attachedTo != null) {
+            StayAttached();
+        }
+
+        mainCam.transform.position = transform.position;
+        mouseSens = mouseSlider.value;
+        mouseText.text = ("Mouse Sensitivity\n" + mouseSens);
+        if (mouseLock) {
+            if (attachedTo != null) {
+                ObjectDirCheck();
+            }
+            MouseLook();
+            //WasdMove();
+        }
+
+        //Mouse Lock
+        if (Input.GetKeyDown(KeyCode.LeftAlt)) {
+            MouseLock();
+        }
+        //Launch Player
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && mouseLock && !launched) {
+            StartLaunch();
+        }
+        if (launched) {
+            LaunchMove();
+        }
+    }
+
+    void MouseLook() {
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+
+        horizontalRotation += Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        mainCam.transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+        //transform.rotation = Quaternion.Euler(transform.rotation.x, horizontalRotation, transform.rotation.z);
+    }
+
+    void WasdMove() {
+
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += wsDir * wasdSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position += -wsDir * wasdSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position += -adDir * wasdSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += adDir * wasdSpeed * Time.deltaTime;
+        }
+    }
+
+    void MouseLock() {
+        mouseLock = !mouseLock;
+        if (mouseLock) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        } else {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    void LaunchMove() {
+        RaycastHit hit;
+
+        if (Physics.SphereCast(transform.position, radius, launchDir, out hit, movSpeed * Time.deltaTime)) {
+            launched = false;
+            transform.position += launchDir * hit.distance;
+            Attach(hit.collider.transform);
+        } else {
+		    transform.position += launchDir * movSpeed * Time.deltaTime;
+        }
 	}
-	
-	void Update () {
-		mainCam.transform.position = transform.position;
-		mouseSens = mouseSlider.value;
-		mouseText.text = ("Mouse Sensitivity\n" + mouseSens);
-		if (mouseLock) {
-			if (attachedToObject != null) {
-				ObjectDirCheck();
-			}
-			MouseLook();
-			WasdMove();
-		}
 
-
-		//Mouse Lock
-		if (Input.GetKeyDown(KeyCode.LeftAlt)) {
-			MouseLock();
-		}
-		//Launch Player
-		if (Input.GetKeyDown(KeyCode.Mouse0) && mouseLock/* && !launched*/) {
-			GrappleDir();
-		}
-		if (launched) {
-			LaunchPlayer();
-		}
-	}
-
-	void MouseLook () {
-		verticalRotation -= Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
-		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
-
-		horizontalRotation += Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
-		mainCam.transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
-		//transform.rotation = Quaternion.Euler(transform.rotation.x, horizontalRotation, transform.rotation.z);
-	}
-
-	void WasdMove () {
-
-		if (Input.GetKey(KeyCode.W)) {
-			transform.position += wsDir * wasdSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.S)) {
-			transform.position += -wsDir * wasdSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.A)) {
-			transform.position += -adDir * wasdSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.D)) {
-			transform.position += adDir * wasdSpeed * Time.deltaTime;
-		}
-	}
-
-	void MouseLock () {
-		mouseLock = !mouseLock;
-		if (mouseLock) {
-			Cursor.lockState = CursorLockMode.Locked;
-			Cursor.visible = false;
-		} else {
-			Cursor.lockState = CursorLockMode.None;
-			Cursor.visible = true;
-		}
-	}
-
-	void LaunchPlayer () {
-		transform.position += launchDir * movSpeed * Time.deltaTime;
-		if (Vector3.Distance(hitPoint.point, transform.position) < 0.5f) {
-			launched = false;
-		}
-	}
-
-	void OnCollisionEnter (Collision c) {
+	/*void OnCollisionEnter (Collision c) {
 		launched = false;
 		transform.rotation = c.transform.rotation;
-		attachedToObject = c.gameObject;
-	}
+        Attach(c.transform);
+	}*/
 
-	void GrappleDir () {
+    void Attach(Transform t) {
+        attachedTo = t;
+        attachPos = t.position;
+        attachScale = t.localScale;
+        attachRot = t.localRotation;
+        Vector3 attachDifference = transform.position - t.position;
+        attachDifference = attachDifference / attachDifference.magnitude * (attachDifference.magnitude - radius);
+        attachDir = Quaternion.Inverse(attachRot)*new Vector3(attachDifference.x/attachScale.x, attachDifference.y / attachScale.y, attachDifference.z / attachScale.z);
+    }
+
+    void StayAttached() {
+        if (attachedTo.rotation != attachRot)
+            attachRot = attachedTo.rotation;
+
+        if (attachedTo.localScale != attachScale)
+            attachScale = attachedTo.localScale;
+
+        if (attachedTo.position != attachPos)
+            attachPos = attachedTo.position;
+
+        Vector3 attachDifference = (attachRot * new Vector3(attachDir.x * attachScale.x, attachDir.y * attachScale.y, attachDir.z * attachScale.z));
+        transform.position = attachPos + attachDifference/attachDifference.magnitude*(attachDifference.magnitude + radius);
+    }
+
+	void StartLaunch () {
 		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        
 		//LayerMask mask = 1 << 8;
 		//Debug.Log("GrappleDir");
 		//if (Physics.Raycast(transform.position, mainCam.transform.forward, out hitPoint)) {
-		if (Physics.Raycast(camRay, out hitPoint)) {
+		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPoint)) {
 			Debug.DrawLine(transform.position, hitPoint.point, Color.red, 5f);
 			if (Vector3.Distance(hitPoint.point, transform.position) > maxGrappleDist) {
 				launchDir = (hitPoint.point - transform.position).normalized;
 				launched = true;
-			} else {
+                attachedTo = null;
+            } else {
 				launchDir = Vector3.zero;
 				launched = false;
 			}
@@ -121,8 +163,8 @@ public class PlayerLookMove : MonoBehaviour {
 	}
 
 	void ObjectDirCheck () {
-		playerToObject = transform.position - attachedToObject.transform.position;
-		Vector3 objScale = attachedToObject.transform.localScale;
+		playerToObject = transform.position - attachedTo.transform.position;
+		Vector3 objScale = attachedTo.transform.localScale;
 
 		if (playerToObject.z > objScale.z / 2 || playerToObject.z < -objScale.z / 2) {
 			Debug.Log("XY controls");
---
+++ Assets/Scripts/TestRot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 90 * Time.deltaTime, 0));
+        transform.position += Vector3.forward * 4 * Time.deltaTime;
+        transform.localScale += Vector3.one * Time.deltaTime;
+	}
+}
