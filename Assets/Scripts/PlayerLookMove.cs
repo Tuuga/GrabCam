dce6167f4cb4da73475ef245e0f420519da7deb9
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2016-01-21 10:53:32 -0800

    Wasd controls removed

--- Assets/Scripts/PlayerLookMove.cs
+++ Assets/Scripts/PlayerLookMove.cs
@@ -8,15 +8,12 @@ public class PlayerLookMove : MonoBehaviour {
     bool launched;
     public float maxGrappleDist;
     public float movSpeed;
-    public float wasdSpeed;
     public float mouseSens;
     public float upDownRange;
     float verticalRotation;
     float horizontalRotation;
     Vector3 launchDir;
     Vector3 playerToObject;
-    Vector3 wsDir;
-    Vector3 adDir;
     public GameObject mainCam;
     public Slider mouseSlider;
     public Text mouseText;
@@ -43,22 +40,19 @@ public class PlayerLookMove : MonoBehaviour {
         mouseSens = mouseSlider.value;
         mouseText.text = ("Mouse Sensitivity\n" + mouseSens);
         if (mouseLock) {
-            if (attachedTo != null) {
-                ObjectDirCheck();
-            }
             MouseLook();
-            //WasdMove();
         }
 
         //Mouse Lock
         if (Input.GetKeyDown(KeyCode.LeftAlt)) {
             MouseLock();
         }
-        //Launch Player
 
+        //Launch Player
         if (Input.GetKeyDown(KeyCode.Mouse0) && mouseLock && !launched) {
             StartLaunch();
         }
+
         if (launched) {
             LaunchMove();
         }
@@ -71,22 +65,6 @@ public class PlayerLookMove : MonoBehaviour {
         horizontalRotation += Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
         mainCam.transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
         //transform.rotation = Quaternion.Euler(transform.rotation.x, horizontalRotation, transform.rotation.z);
-    }
-
-    void WasdMove() {
-
-        if (Input.GetKey(KeyCode.W)) {
-            transform.position += wsDir * wasdSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            transform.position += -wsDir * wasdSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            transform.position += -adDir * wasdSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            transform.position += adDir * wasdSpeed * Time.deltaTime;
-        }
     }
 
     void MouseLock() {
@@ -144,7 +122,6 @@ public class PlayerLookMove : MonoBehaviour {
 
 	void StartLaunch () {
 		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         
 		//LayerMask mask = 1 << 8;
 		//Debug.Log("GrappleDir");
@@ -159,29 +136,6 @@ public class PlayerLookMove : MonoBehaviour {
 				launchDir = Vector3.zero;
 				launched = false;
 			}
-		}
-	}
-
-	void ObjectDirCheck () {
-		playerToObject = transform.position - attachedTo.transform.position;
-		Vector3 objScale = attachedTo.transform.localScale;
-
-		if (playerToObject.z > objScale.z / 2 || playerToObject.z < -objScale.z / 2) {
-			Debug.Log("XY controls");
-			wsDir = Vector3.up;
-			adDir = Vector3.right;
-		}
-
-		if (playerToObject.x > objScale.x / 2 || playerToObject.x < -objScale.x / 2) {
-			Debug.Log("YZ controls");
-			wsDir = Vector3.up;
-			adDir = Vector3.forward;
-		}
-
-		if (playerToObject.y > objScale.y / 2 || playerToObject.y < -objScale.y / 2) {
-			Debug.Log("XZ controls");
-			wsDir = Vector3.left;
-			adDir = Vector3.forward;
 		}
 	}
 }
